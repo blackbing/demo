d89e4f3651c1c13ba26cf17ecef932de3acaf1e6
blackbing <blackbing@gmail.com>
2011-08-30 23:57:07 +0800

    取消進位的trigger方式，改用預先判斷。

--- jq.scrollEffectNumbers/jq.scrollEffectNumbers.js
+++ jq.scrollEffectNumbers/jq.scrollEffectNumbers.js
@@ -10,7 +10,7 @@
             },
             currentDrawPos:{x:0,y:0},
             currentPos:{x:0,y:0},
-            duration:1000,
+            duration:3000,
             timespan:30
         }, options);
 
@@ -24,15 +24,12 @@
             ctx.clearRect(0, 0, opts.canvas.width, opts.canvas.height);
 			var drawX = x;
 			var drawY = y;
-//			if(Math.abs(drawY)> opts.canvas.height){
-				drawY%=(opts.image.height-opts.height);
-//			}
-//			console.log('drawY='+drawY);
+
+			drawY%=(opts.image.height-opts.height);
+
             ctx.drawImage(opts.image, drawX, drawY);
             if(opts.currentDrawPos){
                 var deltaY = Math.abs(Math.abs(opts.currentDrawPos.y) - Math.abs(drawY));
-//                console.log('deltay:'+deltaY);
-                console.log(drawY + '-' + opts.currentDrawPos.y + '=' + deltaY);
                 //如果deltaY > 圖片的一半，代表進位
                 if(deltaY > opts.image.height/2){
 //                    console.log('進位');
@@ -55,10 +52,8 @@
 
             opts.width = img09.width;
             opts.height = Math.round(img09.height/11);
-//            var canvas = $('<canvas width="'+opts.width+'" height="'+opts.height+'"/>').appendTo($elm);
             canvas.attr('width', opts.width);
             canvas.attr('height',opts.height);
-//            alert(0);
             opts.canvas = canvas[0];
             ctx = canvas[0].getContext('2d');
             var y = getNumberPos(opts, opts.initNumber);
@@ -67,8 +62,6 @@
                 0,
                 y
             );
-//            ctx.drawImage(img09, 0, y);
-//			_SELF.go();
 			imgLoaded = true;
         };
         img09.src = opts.path;
@@ -78,13 +71,10 @@
         this.updateOption = function(myoptions){
 
             opts = $.extend(opts, myoptions);
-//            console.log(opts);
         };
         this.add1 = function(){
-            console.log('add1');
             var targetNumber = opts.initNumber+1;
             opts.duration = 500;
-            console.log('targetNumber' + targetNumber);
             this.go({targetNumber:targetNumber});
         };
         this.go = function(args){
@@ -104,22 +94,17 @@
             var y = getNumberPos(opts, opts.initNumber);
             var currentTime = 0;
             var targetNumberPos = getNumberPos(opts, opts.targetNumber);
-			//console.log('targetNumberPos = ' + targetNumberPos);
             var change = targetNumberPos - y;
             var duration = opts.duration;
             var t = +new Date();
             var timespan = opts.timespan;
             opts.interval = setInterval(function(){
-				//console.log(opts.currentPos.y);
-				if(opts.currentPos.y - opts.height < targetNumberPos){
-					//opts.currentPos.y = 0;
-				}
+
                 if(opts.currentPos.y > targetNumberPos){
-                    //var changeY = opts.ease(currentTime, y, change, duration);
+                    
+					
+
                     var changeY = opts.ease(currentTime, y, change, duration);
-//                    var deltaY = Math.abs(changeY-opts.currentPos.y);
-//                    console.log('deltaY:' + deltaY);
-//                    console.log(changeY);
                     draw(ctx, 
                         0,
                         changeY
@@ -130,15 +115,19 @@
                     clearInterval(opts.interval);
                     opts.initNumber = opts.targetNumber;
                     var ct = +new Date();
-                    console.log('t=' + (ct-t));
+
                 }
             }, timespan);
 
         };
     };
     
-
-
+	function padLeft(str, pad, count) {
+		while(str.length<count){
+			str=pad+str;
+		}
+		return str;
+	}
 
 
 
@@ -154,58 +143,20 @@
         var targetNumber = options.targetNumber;
         var MaxNumber = Math.max(initNumber, targetNumber);
         var MaxNumberDigital = MaxNumber.toString().length;
-//        console.log('MaxNumberDigital:' + MaxNumberDigital);
-//        console.log(MaxNumber);
         var canvasNumberStack = [];
+		initNumber = padLeft(initNumber.toString(), '0', MaxNumberDigital);
+		
         for(var i=0; i<MaxNumberDigital; i++){
-            //var num = initNumber.toString().charAt(MaxNumberDigital-i-1) || 0;
+            
             var num = parseInt(initNumber.toString().charAt(i),10) || 0;
-//            console.log(num);
-            if(i==MaxNumberDigital-1){
-                var cnumber = new CanvasNumber(options);
-                canvasNumberStack.push(cnumber);
-            }else{
                 var myoptions = {};
                 myoptions.$elm = this.get(0);
                 myoptions.initNumber = num;
-                myoptions.targetNumber = num + 1;
-                console.log(myoptions);
+				myoptions.targetNumber = parseInt(targetNumber.toString().substring(0, i+1), 10);
                 var bnumber = new CanvasNumber(myoptions);
                 canvasNumberStack.push(bnumber);
-                
-            }
+                bnumber.go();
         }
-        for(var j=canvasNumberStack.length-1; j>=0; j--){
-            if(j===canvasNumberStack.length-1){
-                var extOptions = {
-                    carryOnCallback : function(){
-                    console.log('進位');
-//                        console.log(canvasNumberStack[j-1]);
-//                        var currentOpts = canvasNumberStack[0].getOption();
-//                        alert(currentOpts.targetNumber);
-//                        canvasNumberStack[0].updateOption({
-//                            targetNumber: currentOpts.targetNumber++
-//                        });
-                        canvasNumberStack[0].add1();
-                    }
-                };
-                canvasNumberStack[j].updateOption(extOptions);
-                canvasNumberStack[j].go();
-//                canvasNumberStack[j].add1();
-            }
-        }
-//        console.log(canvasNumberStack);
-
-//        canvasNumberStack[].go();
-
-
-//        while(MaxNumber>1){
-//            var number = Math.floor(MaxNumber%10); 
-//            var cnumber = new CanvasNumber(options);
-//            console.log(number);
-//            canvasNumberStack.push(cnumber);
-//            MaxNumber/=10;
-//        }
     };
 } )( jQuery );
 
