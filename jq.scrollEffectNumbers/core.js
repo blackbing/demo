30d75fb8a13ffdac065ec9e93487ec2c06669a7f
blackbing <blackbing@gmail.com>
2011-08-31 18:22:11 +0800

    加入counter的機制

---
+++ jq.scrollEffectNumbers/core.js
@@ -0,0 +1,23 @@
+/**
+* @author peter
+* @namespace
+* @name olemap
+*/
+(function( window, undefined ){
+
+	var olemap = window.oleamp ? window.olemap : {};
+
+	/**
+	* @memeberOf olemap.util
+	* @namespace
+	*/
+	olemap.util = olemap.util? olemap.util : {};
+
+	window.olemap = olemap;
+
+})( window, undefined );
+
+/**
+* @class
+* @name jQuery
+*/
